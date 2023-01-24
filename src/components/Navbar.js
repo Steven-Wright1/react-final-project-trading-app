8c18f6b2129b4109ba23d9dad544514c6835a971
Steven-Wright1 <steven.wright593@gmail.com>
2023-01-24 19:57:11 +0000

    Added some styling to the homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,13 +8,13 @@ import Navbar from 'react-bootstrap/Navbar';
 
 export const NavigationBar = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar className="Navbar">
         <Container>
           <Navbar.Brand>Trading News</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav  className="me-auto">
-              <Link to="/home" className="nav-link">Watchlist</Link>
+            <Nav className="me-auto">
+              <Link to="/home" className="nav-link" active>Watchlist</Link>
               <Link to="/price-points" className="nav-link">Price Points</Link>
               <Link to="/market-news" className="nav-link">Market News</Link>
               <Link to="/sentiment-analysis" className="nav-link">Sentiment Analysis</Link>
