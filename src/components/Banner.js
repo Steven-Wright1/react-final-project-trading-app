2fe7723ba29772d57851aa21273a1cbc8772e727
Steven-Wright1 <steven.wright593@gmail.com>
2023-01-30 18:46:47 +0000

    banner complete

---
+++ src/components/Banner.js
@@ -0,0 +1,22 @@
+import HeaderPhoto from "../Images/HeaderPhoto.jpg"
+import { Container } from "react-bootstrap";
+
+export const Banner = () =>{
+    return (
+        <div className="header-container">
+
+            <Container>
+            <div className="d-flex align-items-center justify-content-around">
+            <div className="d-flex flex-column">
+                    <h1>Favourites</h1>
+                    <div>Add Favourites Below to Start</div>
+            </div>
+                    <img 
+                    src={HeaderPhoto} 
+                    className="trading-image"
+                    alt="example trading app on mobile device"/>
+            </div>
+            </Container>
+        </div>
+    );
+}
